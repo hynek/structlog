f1e8e111173b03bbd4c8795183d762c46110ee01
Hynek Schlawack <hs@ox.cx>
2013-09-13 13:26:26 +0200

    Start 0.2 cycle

--- structlog/__init__.py
+++ structlog/__init__.py
@@ -18,7 +18,7 @@ Painless structured logging.
 
 from __future__ import absolute_import, division, print_function
 
-__version__ = '0.1.0'
+__version__ = '0.2.0dev'
 
 from structlog._config import (
     configure,
