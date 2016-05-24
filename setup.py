0515833d409d0d16d903e4c8573f335b39d732d2
Hynek Schlawack <hs@ox.cx>
2016-05-24 16:21:57 +0200

    Better changelog re

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ LONG = (
     read("README.rst") + "\n\n" +
     "Release Information\n" +
     "===================\n\n" +
-    re.search("(\d{2}.\d.\d \(.*?\)\n.*?)\n\n\n",
+    re.search("(\d+.\d.\d \(.*?\)\n.*?)\n\n\n----\n\n\n",
               read("CHANGELOG.rst"), re.S).group(1) +
     "\n\n`Full changelog " +
     "<http://www.structlog.org/en/stable/changelog.html>`_.\n\n" +
