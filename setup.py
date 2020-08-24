d549ee6c78f675919d8a4730525e53b1ffedbe3d
Hynek Schlawack <hs@ox.cx>
2020-08-24 09:27:02 +0200

    Add 3.9

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: Implementation :: CPython",
     "Programming Language :: Python :: Implementation :: PyPy",
     "Programming Language :: Python",
