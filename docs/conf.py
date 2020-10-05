69d9e9328d86cdc5e395eadd119c9775ee7343f7
Hynek Schlawack <hs@ox.cx>
2020-10-05 10:16:29 +0100

    Use a transparent logo and use proper text

--- docs/conf.py
+++ docs/conf.py
@@ -129,10 +129,8 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "furo"
-html_theme_options = {
-    "sidebar_hide_name": True,
-}
-html_logo = "_static/structlog_logo_small.png"
+html_theme_options = {}
+html_logo = "_static/structlog_logo_small_transparent.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
