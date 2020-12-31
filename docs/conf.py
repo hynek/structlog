c47662c504f688bf0b15329b9d9bd8548b114f17
Hynek Schlawack <hs@ox.cx>
2020-12-31 10:43:49 +0100

    Stop setting a pygments style

--- docs/conf.py
+++ docs/conf.py
@@ -84,10 +84,6 @@ add_function_parentheses = True
 # unit titles (such as .. function::).
 # add_module_names = True
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
-
 # -- Options for HTML output --------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
