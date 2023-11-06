4d8e11505c2e9d671ac554845825d39051e16ecf
Hynek Schlawack <hs@ox.cx>
2023-11-06 07:01:32 +0100

    Remove accidental change

--- src/structlog/processors.py
+++ src/structlog/processors.py
@@ -332,8 +332,7 @@ class JSONRenderer:
         serializer: Callable[..., str | bytes] = json.dumps,
         **dumps_kw: Any,
     ) -> None:
-        if dumps_kw != {}:
-            dumps_kw.setdefault("default", _json_fallback_handler)
+        dumps_kw.setdefault("default", _json_fallback_handler)
         self._dumps_kw = dumps_kw
         self._dumps = serializer
 
@@ -343,9 +342,7 @@ class JSONRenderer:
         """
         The return type of this depends on the return type of self._dumps.
         """
-        if self._dumps_kw:
-            return self._dumps(event_dict, **self._dumps_kw)
-        return self._dumps(event_dict)
+        return self._dumps(event_dict, **self._dumps_kw)
 
 
 def _json_fallback_handler(obj: Any) -> Any:
