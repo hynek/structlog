a0d568387e63e894aac0cabc12978f37f4e172e1
Hynek Schlawack <hs@ox.cx>
2025-10-13 10:28:03 +0200

    dev: add colors and force_colors properties

--- show_off.py
+++ show_off.py
@@ -25,6 +25,10 @@ log.info("informative!", some_key="some_value")
 log.warning("uh-uh!")
 log.error("omg", a_dict={"a": 42, "b": "foo"})
 log.critical("wtf", what=SomeClass(x=1, y="z"))
+structlog.dev.ConsoleRenderer.get_active().colors = False
+log.info("where are the colors!?", colors="gone")
+structlog.dev.ConsoleRenderer.get_active().colors = True
+log.info("there they are!", colors="back")
 
 
 log2 = structlog.get_logger("another_logger")
