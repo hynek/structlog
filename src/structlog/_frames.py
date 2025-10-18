733c5eaedff9333d19a3ea9bd9f03709cd1a7234
Hynek Schlawack <hs@ox.cx>
2025-10-18 15:42:48 +0200

    Keep order

--- src/structlog/_frames.py
+++ src/structlog/_frames.py
@@ -56,10 +56,10 @@ def _find_first_app_frame_and_name(
     Returns:
         tuple of (frame, name)
     """
+    ignores = ("structlog", *tuple(additional_ignores or ()))
     f = _ASYNC_CALLING_STACK.get(_getframe())
     name = f.f_globals.get("__name__") or "?"
 
-    ignores = ("structlog", *tuple(additional_ignores or ()))
     while name.startswith(ignores):
         if f.f_back is None:
             name = "?"
